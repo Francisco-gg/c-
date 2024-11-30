c748d46df773cf28828da4dccbafbda11adb80b1
Francisco-gg <franciscosampayo2503@gmail.com>
2024-11-29 22:35:52 -0500

    FUNCION SELECCION

--- practica.cpp
+++ practica.cpp
@@ -3,7 +3,23 @@
 
 using namespace std;
 
+void seleccion(){
+
+    int sel;
+
+    printf("\t\t ELIJE UN PERSONAJE\n\n");
+
+    printf("#################### \n");
+    printf("1. LUCHADOR \n");
+    printf("2. MAGO \n");
+    printf("3. TANQUE \n");
+    printf("#################### \n\n");
+
+    printf("ELIJE: ");scanf("%d",&sel);
+}
+
 int main(){
 
-    
+
+   
 }
